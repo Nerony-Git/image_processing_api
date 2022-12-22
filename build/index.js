ecc4deb5c47a51c13e39e1268eb437c1cef139ee
George Amuzu <gnamuzu@gmail.com>
2022-12-22 12:51:12 +0000

    made a few changes to check if the test

--- build/index.js
+++ build/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = (0, express_1.default)();
 var port = 3000;
-app.get("/api", function (req, res) {
+app.get("/", function (req, res) {
     res.send("Hello, Udacity Test!!");
 });
 app.listen(port, function () {
