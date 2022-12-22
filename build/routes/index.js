c2abd16f8a56731be18828aba1d0c50e25443e6d
George Amuzu <gnamuzu@gmail.com>
2022-12-22 13:19:43 +0000

    created routes path

--- build/routes/index.js
+++ build/routes/index.js
@@ -4,11 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var index_1 = __importDefault(require("./routes/index"));
-var app = (0, express_1.default)();
-var port = 3000;
-app.use("/api", index_1.default);
-app.listen(port, function () {
-    console.log("server started at localhost: ", port);
+var image_1 = __importDefault(require("./api/image"));
+var routes = express_1.default.Router();
+routes.get("/", function (req, res) {
+    res.send("Hello, Udacity Main Test!!");
 });
-exports.default = app;
+routes.use("/image", image_1.default);
+exports.default = routes;
