d5dd8e4ddecb29f6cbb00048fa597ed9d214b0fd
George Amuzu <gnamuzu@gmail.com>
2022-12-23 07:28:22 +0000

    A sample code to get query parameters from client

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -2,8 +2,13 @@ import express from "express";
 const images = express.Router();
 
 images.get("/", (req, res) => {
+    const filename = req.query.filename;
+    const width = req.query.width;
+    const height = req.query.height;
+
+
+    console.log("File Name: " + filename + ", Width: " + width + ", Height: " + height);
     res.send("Image Folder");
 });
-
 
 export default images;
