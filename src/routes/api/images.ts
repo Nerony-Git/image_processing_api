7bfb31cb60db6f050a9f4742f53ade4610879762
George Amuzu <gnamuzu@gmail.com>
2022-12-23 08:24:00 +0000

    edited to check if a file exist in the images data

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,15 +1,26 @@
 import express from "express";
+import fs from "fs";
+//import { promises as fsPromises } from "fs";
+
 const images = express.Router();
 
 images.get("/", (req, res) => {
     const filename = req.query.filename as string;
     const width = req.query.width as string;
     const height = req.query.height as string;
+    const file_path = "./images_data/image_input/" + filename + ".jpg" as string;
+    
 
 
     //Validate inputs
     if(filename !== undefined && width !== undefined && height !== undefined) {
         res.send("File Name: " + filename + ", Width: " + width + ", Height: " + height);
+        // Check if file exist in images_data folder
+        if(fs.existsSync(file_path)) {
+            console.log(filename + " exists in images data folder");
+        }else {
+            console.log(filename + " does not exists in images data folder");
+        }
     }
     else if(filename === undefined && width !== undefined && height !== undefined) {
         res.send("Please input file name!")
