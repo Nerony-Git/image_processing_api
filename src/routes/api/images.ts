8691df31ced0140d0de403f6271f7fe877902bdf
George Amuzu <gnamuzu@gmail.com>
2022-12-23 07:56:20 +0000

    validating query parameters

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -2,13 +2,35 @@ import express from "express";
 const images = express.Router();
 
 images.get("/", (req, res) => {
-    const filename = req.query.filename;
-    const width = req.query.width;
-    const height = req.query.height;
+    const filename = req.query.filename as string;
+    const width = req.query.width as string;
+    const height = req.query.height as string;
+
+
+    //Validate inputs
+    if(filename !== undefined && width !== undefined && height !== undefined) {
+        res.send("File Name: " + filename + ", Width: " + width + ", Height: " + height);
+    }
+    else if(filename === undefined && width !== undefined && height !== undefined) {
+        res.send("Please input file name!")
+    }else if(filename !== undefined && width === undefined && height !== undefined) {
+        res.send("Please input width!")
+    }else if(filename !== undefined && width !== undefined && height === undefined) {
+        res.send("Please input height!")
+    }
+    else if(filename === undefined && width === undefined && height !== undefined) {
+        res.send("Please input file name and width!")
+    }else if(filename === undefined && width !== undefined && height === undefined) {
+        res.send("Please input file name and height!")
+    }else if(filename !== undefined && width === undefined && height === undefined) {
+        res.send("Please input width and height!")
+    }else if(filename === undefined && width === undefined && height === undefined) {
+        res.send("Please input values for filename, width and height")
+    }
 
 
     console.log("File Name: " + filename + ", Width: " + width + ", Height: " + height);
-    res.send("Image Folder");
+    //res.send("Image Folder");
 });
 
 export default images;
