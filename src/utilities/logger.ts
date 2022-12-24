69041c33359fb3827ac253cdf90538e779f84878
George Amuzu <gnamuzu@gmail.com>
2022-12-24 14:07:07 +0000

    updated logger file

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-const logger = (req: express.Request, res: express.Response, next: Function): void => {
+const logger = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
     const url = req.url;
     console.log(url, "was visited");
     next();
