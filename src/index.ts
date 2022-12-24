b39cabb4c9916155a7d4f4ff8e3d6bd8e4776415
George Amuzu <gnamuzu@gmail.com>
2022-12-24 08:52:06 +0000

    updated to implement async await

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import routes from "./routes/index";
+import logger from "./utilities/logger";
+import * as verify from "./utilities/validation";
+
 
 const app = express();
 const port = 3000;
 
-app.use("/api", routes);
+app.use(logger, routes);
 
-app.listen(port, () => {
+app.listen(port, async(): Promise<void> => {
+    await verify.default.check_output_folder();
     console.log("server started at localhost: ", port);
 });
 
