775dacd741973707e29fa7889bfaca3eeee348df
George Amuzu <gnamuzu@gmail.com>
2022-12-24 08:45:30 +0000

    function to get path of app

---
+++ src/utilities/app_path.ts
@@ -0,0 +1,12 @@
+import * as path from "path";
+//import main = require('require-main-filename');
+
+const file_path = path.resolve(__dirname, "../../images_data/image_input");
+const output_file_path = path.resolve(__dirname, "../../images_data/image_output");
+
+
+export default {
+    output_file_path,
+    file_path,
+    path
+};
